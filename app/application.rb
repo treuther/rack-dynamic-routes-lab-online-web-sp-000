69e76260a203d80455d337544d6b1120bb9eca64
Tracy Reuther <tracyreuther@gmail.com>
2019-08-25 21:02:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match =="/item/"
-      resp.write "#{item}"
+      item_name = req.path.split("/item/")
     else
       resp.write "Route not found"
       resp.status = 404
